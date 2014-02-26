894b6fba695fcb9e993ad5f29df1b28f86bc4e07
DrNorton <dimaivanov1@mail.ru>
2014-02-26 18:07:17 +0400

    Initial commit

---
+++ LocationTestTask/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Phone.Controls;
+
+namespace LocationTestTask.UI
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
