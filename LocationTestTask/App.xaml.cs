84fc7fa9942d1fd83ab83611485dac7c83fa728e
dimaivanov1@mail.ru <rianon>
2014-02-28 21:18:53 +0400

    Небольшие поправки

--- LocationTestTask/App.xaml.cs
+++ LocationTestTask/App.xaml.cs
@@ -5,19 +5,11 @@ namespace LocationTestTask.UI
 {
     public partial class App : Application
     {
-        public PhoneApplicationFrame RootFrame { get; private set; }
-
         public App()
         {
             InitializeComponent();
             
         }
-
-        void App_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
-        {
-            
-        }
-
   
     }
 }
